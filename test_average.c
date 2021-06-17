322bf509f25e34da1edbd02ab8cbab0e09903c8e
Athif-190101 <85243603+Athif-190101@users.noreply.github.com>
2021-06-17 03:45:56 -0700

    Update test_average.c

--- test_average.c
+++ test_average.c
@@ -12,3 +12,12 @@ int main(void) {
   assert (average(y, 1) == 5.0);
   printf("Average: passed\n");
   }
+
+float average(int a[],int n)
+{int av,sum;
+ sum=0;
+  for(int i=0;i<n;i++)
+   sum+=a[i];
+  av=(sum/n);
+  return av;
+}
