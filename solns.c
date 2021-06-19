4a968b4c0a5d7aedb1fdd9fc9c8ac7b5d78b1029
Athif-190101 <85243603+Athif-190101@users.noreply.github.com>
2021-06-19 12:32:05 -0700

    Update solns.c

--- solns.c
+++ solns.c
@@ -36,27 +36,35 @@ for(int i=0;i<n;i++)
 return s;
 }
 
-int mode(int a[],int n)
-{int x,l=0;
- int ar[],br[];
+int mode(int ar[],int n)
+  {int x,br[20],cr[20],y,l=0,x1;
   for(int i=0;i<n;i++)
-  {x=ar[i];
-    for(int j=i+1 j<n;j++)
-     {if(x==ar[j])
-        l++;
-      x=ar[i];
-      l=br[i];
-  }
-  }
-   int s=br[0],n;
-  for(i=0;i<n;i++)
-     {if(s<br[i])
-        s=br[i];
-        n=i
+    {if(x!=ar[i])
+     {
+    x=ar[i];
+      for(int j=1;j<n;j++)
+       {if(x==ar[j])
+         {x=br[i];
+          l++;
+         }
+        }
+       }
+      else
+       continue;
+       cr[i]=l;
      }
-     return ar[n];
-}
-  }
+    x1=br[0];
+    for(int i=0;i<n;i++)
+     {if(x1<br[i])
+       x1=br[i];
+     }
+      
+     for(int i=0;i<n;i++)
+       {if(x1==br[i])
+         y=cr[i];
+       }
+   return y;
+   }
 
   int factor(int n,int ar[])
    {int flag=0,i,j,k=0,y=0,num,l=2;
